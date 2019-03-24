ef484e0cf8f9f7c9fad647cc2279d1dca302ee8e
Otash8888 <otabekkamalov@yahoo.com>
2019-03-24 17:32:26 -0400

    didMount implied1

--- fhv-helper/src/components/Fetch.js
+++ fhv-helper/src/components/Fetch.js
@@ -4,21 +4,27 @@ class Fetch extends Component {
   state = {
     fetched:{}
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      this.setState({
+        fetched:JSON.stringify(myJson)
+      })
+
+    })
+  }
+  
   render() {
 
-    componentDidMount() {
-      fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(myJson) {
-        JSON.stringify(myJson);
-      })
-    }
-    
+
+
     return (
       <div>
-        <h1>"Airport Info" {airportInfoFetch()}</h1>
+        <h1>"Airport Info" {this.state.fetched}</h1>
 
       </div>
     );
