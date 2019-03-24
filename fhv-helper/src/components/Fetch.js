e5df0296c6bd0d33959507bd29b43d27c0401a2a
Otash8888 <otabekkamalov@yahoo.com>
2019-03-24 17:18:37 -0400

    test fetch

--- fhv-helper/src/components/Fetch.js
+++ fhv-helper/src/components/Fetch.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 
 class Fetch extends Component {
+
   render() {
+
+    let airportInfoFetch = () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      })
+    }
+
     return (
       <div>
-        <h1>"Airport Info"</h1>
+        <h1>"Airport Info" {airportInfoFetch()}</h1>
+
       </div>
     );
   }
