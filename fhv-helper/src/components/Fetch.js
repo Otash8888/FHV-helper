5015dcac13506e3481c25f1caeeedc08358d2af5
Otash8888 <otabekkamalov@yahoo.com>
2019-03-24 16:48:13 -0400

    Airport info started

--- fhv-helper/src/components/Fetch.js
+++ fhv-helper/src/components/Fetch.js
@@ -1,1 +1,13 @@
 import React, { Component } from 'react';
+
+class Fetch extends Component {
+  render() {
+    return (
+      <div>
+        <h1>"Airport Info"</h1>
+      </div>
+    );
+  }
+}
+
+export default Fetch;
