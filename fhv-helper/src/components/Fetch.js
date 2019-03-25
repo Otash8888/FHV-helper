93b11bcc189af675da5cd940a85c4923ed096d3f
Otash8888 <otabekkamalov@yahoo.com>
2019-03-25 13:21:21 -0400

    api call and saving upcoming flights

--- fhv-helper/src/components/Fetch.js
+++ fhv-helper/src/components/Fetch.js
@@ -1,37 +1,42 @@
 import React, { Component } from 'react';
 
+let my_key = '4221d2-6285ff'
+let api_jfk_arrivals = `http://aviation-edge.com/v2/public/timetable?key=${my_key}&iataCode=JFK&type=arrival`
+
 class Fetch extends Component {
   constructor(props) {
     super(props);
-    this.state = { fetched:{} };
+    this.state = { jfk_arrivals:[] };
   }
 
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    fetch(api_jfk_arrivals)
     .then(function(response) {
       return response.json();
     })
     .then(myJson => {
-
-      this.setState({
-        fetched:JSON.stringify(myJson)
+      // filtering for upcoming flights
+      let upcomingFlights = myJson.filter((flightObj) => {
+        let timeArrival = new Date(flightObj.arrival.estimatedTime)
+        let timeNow = new Date()
+        return timeNow < timeArrival
       })
-      console.log(this.state.fetched)
+      //saving in state
+      this.setState({
+        jfk_arrivals:upcomingFlights
+      },() => {
+        console.log(this.state.jfk_arrivals)
+      })
 
     })
   }
 
 
   render() {
-
-
-
     return (
       <div>
         <h1>"Airport Info" </h1>
-        <p> {`${this.state.fetched}`} </p>
-
       </div>
     );
   }
