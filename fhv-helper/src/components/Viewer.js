69417d8428cbf0af0e4fe8e8885b1948a80917a2
Otash8888 <otabekkamalov@yahoo.com>
2019-03-31 22:35:50 -0400

    minor

--- fhv-helper/src/components/Viewer.js
+++ fhv-helper/src/components/Viewer.js
@@ -3,10 +3,7 @@ import React, { Component } from 'react';
 const Viewer = ({ terminal })=> {
   console.log(terminal)
   return (
-
-
-       <li>{terminal.airline.name} </li>
-
+    <li>{terminal.airline.name} </li>
   )
 }
 
